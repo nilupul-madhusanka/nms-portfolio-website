08151897d799d51ab896adbd3030b41962298351
nilupul-madhusanka <nilupulmadhusanka2001m31@gmail.com>
2025-04-27 09:44:53 +0530

    2

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -37,10 +37,10 @@ const EmailSection = () => {
     >
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
       <div className="z-10">
-        <h5 className="text-xl font-bold text-white my-2">Let's Connect</h5>
+        <h5 className="text-xl font-bold text-white my-2">Let&apos;s Connect</h5>
         <p className="text-[#ADB7BE] mb-4 max-w-md">
-          I'm currently looking for new opportunities, my inbox is always
-          open. Whether you have a question or just want to say hi, I'll
+          I&apos;m currently looking for new opportunities, my inbox is always
+          open. Whether you have a question or just want to say hi, I&apos;ll
           try my best to get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
@@ -114,7 +114,7 @@ const EmailSection = () => {
               onChange={handleChange}
               required
               className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-              placeholder="Let's talk about..."
+              placeholder="Let&apos;s talk about..."
             ></textarea>
           </div>
           <button
@@ -127,6 +127,7 @@ const EmailSection = () => {
       </div>
     </section>
   );
+  
 };
 
 export default EmailSection;
