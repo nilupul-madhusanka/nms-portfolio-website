7a5f7bc124a39734cc8196c7133554df42bf83f1
nilupul-madhusanka <nilupulmadhusanka2001m31@gmail.com>
2025-12-13 18:51:21 +0530

    Fixed Issues for Deployment

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    unoptimized: false,
+  },
+  // Ensures trailing slashes are consistent
+  trailingSlash: false,
+}
 
 module.exports = nextConfig
